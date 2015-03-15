d554dee38fcfb23b9a99afdd5e06ac68f83a3d82
Eric Barb <eric.barb@gmail.com>
2015-03-15 17:46:12 -0400

    More Tutorial5.R Updates
    Include some stuff from Lessons 3 & 4

--- Tutorial5.R
+++ Tutorial5.R
@@ -82,15 +82,38 @@ library(RColorBrewer)
 #Similar to aggregate, the field you caculating on to the left of the tilda, and the fields to compare_
 #on the right, followed by the data set, then the method
 #fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=train, method="class")
+#If you want to adjust how complex the decsion tree can get, you can add a control to the end of it_
+#The two settings for this are minsplit and cp(comlexity)
+#fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=train, method="class", control=rpart.control(minsplit=2, cp=0))
 
 #Now you can create some visuals using what you just created
 #fancyRpartPlot(fit)
+
+#If you want to create an interactive tree of plots where you can X out some trees do this:
+#new.fit <- prp(fit,snip=TRUE)$obj
+#fancyRpartPlot(new.fit)
+#When you are done X'ing them out, TYpe "Quit"  
 
 #After analyzing the data, lets create a prediction based on the decision tree (fit) that we just created
 #Prediction <- predict(fit, test, type = "class")
 #Above we have a new "variable" called Prediction which is creating a prediction "predict" based on the_
 #datatree "fit", applying it to the data set "test", with a type of "class" (for 1's and 0's)
 
+
+#Combing Data Sets : Each data set must have the same fields.
+#So if starting from scrath, create a Survived Colum in test
+  #test$Survived <- NA
+#Now to get down to business. 
+  #combi <- rbind(train, test)
+#This creates a new dataset called combi
+
+#Split up the Name so that we can grab the title
+#The following usesstrsplit, followed by the dataset$field, what to split on (comma and period), then_
+#the array type stuff.  1 gets the name, 2 gets the 2nd of three separations, or 2nd value of the array
+#strsplit(combi$Name[1], split='[,.]')[[1]][2]
+
+#To apply the above to all objects or records of the dataset
+#combi$Title <- sapply(combi$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][2]})
 
 
 
